ae2bd6e80c449e38e62e2e87aad92cd3e9cf5bf6
Luka Sijic <23730024+luka-sijic@users.noreply.github.com>
2025-07-20 04:04:30 -0700

    fixes for deployment

--- frontend/src/services/service.tsx
+++ frontend/src/services/service.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.PUBLIC_API || "http://127.0.0.1:8081",
+  baseURL: process.env.NEXT_PUBLIC_API,
   withCredentials: true,
 });
 
