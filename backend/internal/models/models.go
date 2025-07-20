9aefffbcba608b4402331889f37bcf5853c23a8b
Luka Sijic <23730024+luka-sijic@users.noreply.github.com>
2025-07-19 23:16:47 -0700

    fixes

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -9,6 +9,7 @@ import (
 type Message struct {
 	Type     string            `json:"type"`
 	Username string            `json:"user,omitempty"`
+	User2    string            `json:"user2"`
 	Content  string            `json:"content,omitempty"`
 	Users    map[string]string `json:"users,omitempty"`
 	Log      []string          `json:"log,omitempty"`
--- backend/internal/pubsub/publish.go
+++ backend/internal/pubsub/publish.go
@@ -4,9 +4,12 @@ import (
 	"app/internal/models"
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 
 	"app/internal/database"
+
+	"github.com/redis/go-redis/v9"
 )
 
 // publish sends a message into the Pub/Sub topic.
@@ -18,5 +21,22 @@ func Publish(msg models.Message) {
 	}
 	if count == 0 {
 		log.Println("warning: no subscribers for topic")
+	}
+	var key string
+	if msg.Username > msg.User2 {
+		key = fmt.Sprintf("conversation:%s:%s", msg.Username, msg.User2)
+	} else {
+		key = fmt.Sprintf("conversation:%s:%s", msg.User2, msg.Username)
+	}
+	fmt.Println("Key: ", key)
+	_, err = database.RDB.XAdd(context.Background(), &redis.XAddArgs{
+		Stream: key,
+		Values: map[string]interface{}{
+			"username": msg.Username,
+			"message":  msg.Content,
+		},
+	}).Result()
+	if err != nil {
+		log.Println(err)
 	}
 }
